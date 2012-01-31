44562ab7a9a7fae188fadb4607ed5f32111e8770
Thomas Dark <tom@vaultrms.com>
2012-01-31 14:12:16 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VaultRMS::Application.config.secret_token = 'd19b45bbf6031ab1ffa56dbce5ff61ccf7b42b9e4d3033a5f40a82d3b22c3f8433fead2d5b5f5d9bec2d91a7f1f00436d680a6ad0a14778b9ee4af2089645ffb'
